08456fbf4fbd9c06d9c4d389e3601b7e0d3487cb
RHU4N <rhuan3003@gmail.com>
2024-09-11 21:19:58 -0300

    Terminando API e comunicando com pg web

--- Aula03-Spring/tasklist/src/main/java/br/edu/fateccotia/isw029/tasklist/repository/TokenRepository.java
+++ Aula03-Spring/tasklist/src/main/java/br/edu/fateccotia/isw029/tasklist/repository/TokenRepository.java
@@ -1,5 +1,7 @@
 package br.edu.fateccotia.isw029.tasklist.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import br.edu.fateccotia.isw029.tasklist.model.Token;
 
 @Repository
 public interface TokenRepository extends CrudRepository<Token, Integer> {
+
+	Optional<Token> findByToken(String token);
 	
 }
